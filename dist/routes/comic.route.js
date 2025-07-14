49436adc6983ef77d3602cd53170b239608e9872
nagamiuko <nagamiuko@gmail.com>
2025-07-14 23:49:59 +0700

    update vercel

--- dist/routes/comic.route.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const linkComic_1 = require("@/controllers/comic/linkComic");
-const express_1 = require("express");
-const router = (0, express_1.Router)();
-router.post("/comic/like", linkComic_1.likeComic);
-router.post("/comic/unlike", linkComic_1.unlikeComic);
-exports.default = router;
