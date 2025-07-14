0e1d9874e2ffb4c4895d803578cd105f0c1c0a16
nagamiuko <nagamiuko@gmail.com>
2025-07-14 23:31:35 +0700

    update api config

--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,27 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import cors from "cors";
 import routes from "@/routes/route";
 import user from "@/routes/user.route";
-import product from "@/routes/product.route"
-import comments from "@/routes/comment.route"
-import comic from "@/routes/comic.route"
+import product from "@/routes/product.route";
+import comments from "@/routes/comment.route";
+import comic from "@/routes/comic.route";
 
 const app = express();
 
-app.use(cors({
-    origin: ['https://laughing-space-giggle-x7v5gq6w99vc64jr-8080.app.github.dev'],
+app.use(
+  cors({
+    origin: [
+      "https://laughing-space-giggle-x7v5gq6w99vc64jr-8080.app.github.dev",
+    ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
-},
-));
+  })
+);
 app.use(express.json());
 
+app.get("/", (req: Request, res: Response) => {
+  res.send("Welcome to api service");
+});
 app.use("/api", routes);
 app.use("/api", user);
 app.use("/api", product);
